a813a01f25cb5a38caa51605374f346e1debca6f
ALYSSA_MAPLES <armaples@ucsc.edu>
2022-10-30 12:24:47 -0400

    Password Generation now functioning

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Variables
 var generateBtn = document.querySelector("#generate");
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var numericChar = "0123456789";
+var specialChar = "!#$%&'()*+,-./:;<=>?@^_`~";
 var inputPrompt = "Input a length for your password (8-128 characters): ";
 var upperPrompt = "Press OK to include uppercase characters in the password.";
 var lowerPrompt = "Press OK to include lowercase characters in the password.";
@@ -13,83 +17,117 @@ var tryAgain = "Press OK to try again."
 // Function for collecting user input to generate password
 function generatePassword() {
     // Calling functions 
-    characterNum();
-    characterType();
-    // call function to generate password
+    // characterNum();
+    // characterType();
+    // userPassword();
 
   // Function for user input on password length
-  function characterNum() {
+  // function characterNum() {
     // Prompt for PW length
-    var passwordLength = window.prompt(inputPrompt);
-    console.log(passwordLength);
+  var passwordLength = window.prompt(inputPrompt);
+  console.log(passwordLength);
     
     // Loop if not between 8-128
-    while (passwordLength < 8 || passwordLength > 128) {
-      alert("ERROR: Password must be between 8 and 128 characters. " + tryAgain);
-      var passwordLength = window.prompt(inputPrompt);
-      console.log(passwordLength);
-    } 
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("ERROR: Password must be between 8 and 128 characters. " + tryAgain);
+    var passwordLength = window.prompt(inputPrompt);
+    console.log(passwordLength);
+  } 
     
-    // Confirmation for Password Length
-    var confirmLength = confirm("You have chosen " + passwordLength + " characters for your password. " + pressOK);
+  // Confirmation for Password Length
+  var confirmLength = alert("You have chosen " + passwordLength + " characters for your password. " + pressOK);
+  
+  // characterType();
+  // function characterType() {
+  // Prompt for Uppercase Characters
+  var upperCase = confirm(upperPrompt);
+  console.log(upperCase);
+    
+  // Confirmation Alert for Uppercase Characters
+  if (upperCase) {
+    alert("Password will include uppercase characters. " + pressOK);
+  } else {
+    alert("Password will not include uppercase characters. " + pressOK);
+  } 
+    
+  // Prompt for Lowercase Characters
+  var lowerCase = confirm(lowerPrompt);
+  console.log(lowerCase);
+
+  // Confirmation Alert for Lowercase Characters
+  if (lowerCase) {
+    alert("Password will include lowercase characters. " + pressOK);
+  } else {
+    alert("Password will not include lowercase characters. " + pressOK);
   }
 
-  // Function for user input on character types included in password
-  function characterType() {
-    // Prompt for Uppercase Characters
-    var upperCase = confirm(upperPrompt);
-    console.log(upperCase);
+  // Prompt for Numeric Characters
+  var numericVal = confirm(numericPrompt);
+  console.log(numericVal);
     
-    // Confirmation Alert for Uppercase Characters
-    if (upperCase) {
-      alert("Password will include uppercase characters. " + pressOK);
-    } else {
-      alert("Password will not include uppercase characters. " + pressOK);
-    } 
-    
-    // Prompt for Lowercase Characters
-    var lowerCase = confirm(lowerPrompt);
-    console.log(lowerCase);
-
-    // Confirmation Alert for Lowercase Characters
-    if (lowerCase) {
-      alert("Password will include lowercase characters. " + pressOK);
-    } else {
-      alert("Password will not include lowercase characters. " + pressOK);
-
-    }
-
-    // Prompt for Numeric Characters
-    var numericChar = confirm(numericPrompt);
-    console.log(numericChar);
-    
-    // Confirmation Alert for Numeric Characters
-    if (numericChar) {
-      alert("Password will include numeric characters. " + pressOK);
-    } else {
-      alert("Password will not include numeric characters. " + pressOK);
-    }
-
-    // Prompt for Special Characters
-    var specialChar = confirm(specialPrompt);
-    console.log(specialChar);
-
-    // Confirmation Alert for Special Characters
-    if (specialChar) {
-      alert("Password will include special characters. " + pressOK);
-    } else {
-      alert("Password will not include special characters.");
-    }
-
-    // If statement for when no characters are selected
-    if (!upperCase && !lowerCase && !numericChar && !specialChar) {
-      alert("ERROR: You must choose at least one type of character. " + tryAgain);
-      characterType();
-    } 
+  // Confirmation Alert for Numeric Characters
+  if (numericVal) {
+    alert("Password will include numeric characters. " + pressOK);
+  } else {
+    alert("Password will not include numeric characters. " + pressOK);
   }
 
-    // Generate Password based on user input 
-}
+  // Prompt for Special Characters
+  var specialVal = confirm(specialPrompt);
+  console.log(specialVal);
+
+  // Confirmation Alert for Special Characters
+  if (specialVal) {
+    alert("Password will include special characters. " + pressOK);
+  } else {
+    alert("Password will not include special characters.");
+  }
+
+  // Alert when no characters are selected
+  while (!upperCase && !lowerCase && !numericVal && !specialVal) {
+    alert("ERROR: You must choose at least one type of character. " + tryAgain);
+    // characterType();
+  } 
+
+  // Generate Password based on user input 
+  alert("Generating Password...");
+  console.log(upperCase, lowerCase, numericVal, specialVal);
+
+  var passwordCriteria = [];
+
+  // If statements for password criteria combinations
+  if (upperCase) {
+    var passwordCriteria = passwordCriteria.concat(Array.from(upperChar));
+    console.log(passwordCriteria);
+  } 
+    
+  if (lowerCase) {
+    var passwordCriteria = passwordCriteria.concat(Array.from(lowerChar));
+    console.log(passwordCriteria);
+  } 
+    
+  if (numericVal) {
+    var passwordCriteria = passwordCriteria.concat(Array.from(numericChar));
+    console.log(passwordCriteria);
+  } 
+    
+  if (specialVal) {
+    var passwordCriteria = passwordCriteria.concat(Array.from(specialChar));
+    console.log(passwordCriteria);
+  } 
+  
+  const userPassword = [];
+
+  for (let i = 0; i < passwordLength; i++) {
+    const passwordGen = passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
+    userPassword.push(passwordGen);
+    console.log(userPassword);
+  } 
+
+  return userPassword.join("");
+    
+  }
+    
 
 // Function for writing password to the #password input
 function writePassword() {
